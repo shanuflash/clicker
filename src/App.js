9df3b01cc23a23ea9f7d1abd4ea74a611a0a4568
Shanu S <shanuflash@gmail.com>
2023-01-07 14:05:51 +0530

    add reset function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, React } from "react";
 
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import ClearIcon from "@mui/icons-material/Clear";
 
 function App() {
   const [count, setcount] = useState(0);
@@ -30,6 +31,17 @@ function App() {
           onClick={() => setcount(count - 1)}
         >
           <RemoveIcon style={{ color: "white" }} />
+        </button>
+        <button
+          disabled={count === 0}
+          style={{
+            borderRadius: "100%",
+            width: "100px",
+            height: "100px",
+          }}
+          onClick={() => setcount(0)}
+        >
+          <ClearIcon style={{ color: "white" }} />
         </button>
       </div>
       <div className="Count">{count}</div>
